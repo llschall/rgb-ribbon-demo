1badf535a49215286c3f1ea8e82df1f90e480b12
llschall <llschall@gmx.com>
2025-07-20 21:54:21 +0200

    [Ino] Integrate FastLED library

--- ino/rgb-ribbon/rgb-ribbon.ino
+++ ino/rgb-ribbon/rgb-ribbon.ino
@@ -9,7 +9,22 @@
 // Use Ardwloop 0.3.3
 #include <Ardwloop.h>
 
+// Use FastLED by Daniel Garcia
+// Version 3.10.1
+#include <FastLED.h>
+
+
+// How many leds in your strip?
+#define NUM_LEDS 5
+
+#define DATA_PIN 3
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
 void setup() {
+
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(700);
@@ -33,8 +48,16 @@ void loop() {
 
   if (v == 1) {
     digitalWrite(LED_BUILTIN, HIGH);
+    leds[0] = CRGB::Red;
   } else {
     digitalWrite(LED_BUILTIN, LOW);
+    leds[0] = CRGB::Green;
   }
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+
+  }
+  FastLED.show();
+
   delay(99);
 }
